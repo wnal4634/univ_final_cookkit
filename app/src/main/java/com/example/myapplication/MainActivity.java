d7c107f3305ef89d2ae381edf5786a8ac464d055
2005877 <ccom0525ii@naver.com>
2022-04-28 19:17:59 +0900

    2022-04-28 하단바, index,검색창, mypage

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment_index fragment_index;
+    Fragment_search fragment_search;
+    Fragment_mypage fragment_mypage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment_index = new Fragment_index();
+        fragment_search = new Fragment_search();
+        fragment_mypage = new Fragment_mypage();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_index).commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_index).commit();
+                        return true;
+                    case R.id.item2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_search).commit();
+                        return true;
+                    case R.id.item3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_mypage).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
