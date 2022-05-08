0a761328176a8f43189218e198a7829f4eb06e5f
2005877 <ccom0525ii@naver.com>
2022-05-08 22:35:17 +0900

    좋아요, 추천페이지

--- app/src/main/java/com/example/myapplication/Fragment_mypage.java
+++ app/src/main/java/com/example/myapplication/Fragment_mypage.java
@@ -14,6 +14,8 @@ public class Fragment_mypage extends Fragment {
 
     private View view;
     private Button btn_noti;
+    private Button btn_like;
+    private Button btn_reco;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +27,25 @@ public class Fragment_mypage extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(),noticeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        btn_like = (Button) view.findViewById(R.id.button15);
+        btn_like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),LikeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+        btn_reco = (Button) view.findViewById(R.id.button16);
+        btn_reco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),RecoActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
             }
