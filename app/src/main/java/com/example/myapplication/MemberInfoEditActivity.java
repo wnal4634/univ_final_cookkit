2d729c89b0b9ed12ca85f484286d7cc2f4ec3fac
이다혜(B) <dlwlgns112201@naver.com>
2022-05-16 19:40:02 +0900

    2022-05-16 회원가입, 회원정보 수정페이지

--- app/src/main/java/com/example/myapplication/MemberInfoEditActivity.java
+++ app/src/main/java/com/example/myapplication/MemberInfoEditActivity.java
@@ -3,12 +3,15 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MemberInfoEditActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_info_edit);
+
     }
 }
