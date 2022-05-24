c8d36a863365ae0accddd123cb88ffc61dc489d8
2005897 <wnal4634@naver.com>
2022-05-24 15:57:50 +0900

    디자인 수정

--- app/src/main/java/com/example/myapplication/MemberInfoEditActivity.java
+++ app/src/main/java/com/example/myapplication/MemberInfoEditActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MemberInfoEditActivity extends AppCompatActivity {
@@ -13,5 +17,16 @@ public class MemberInfoEditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_info_edit);
 
+
+        ImageButton btn_back_mypage = (ImageButton) findViewById(R.id.back_mypage);
+        btn_back_mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        MemberInfoEditActivity.this, Fragment_mypage.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                finish();
+            }
+        });
     }
 }
