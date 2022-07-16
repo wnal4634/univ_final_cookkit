1e0d034cce65c3e7ebc23e30934c1b072144d51a
2005877 <ccom0525ii@naver.com>
2022-07-16 21:31:12 +0900

    레시피 사진

--- app/src/main/java/com/example/myapplication/RecipewriteActivity.java
+++ app/src/main/java/com/example/myapplication/RecipewriteActivity.java
@@ -2,29 +2,40 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class RecipewriteActivity extends AppCompatActivity {
 
-    private static final int REQUST_CODE=0;
+//    private static int REQUST_CODE = 0;
 
     ImageView main, image1, image2, image3, image4, image5, image6;
-
+    EditText recipe_title, recipe_mat, recipe_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,49 +75,228 @@ public class RecipewriteActivity extends AppCompatActivity {
         image4 = findViewById(R.id.recipegallery4);
         image5 = findViewById(R.id.recipegallery5);
         image6 = findViewById(R.id.recipegallery6);
-
+        recipe_title = findViewById(R.id.recipe_title);
+        recipe_mat = findViewById(R.id.recipe_mat);
+        recipe_text = findViewById(R.id.recipe_text);
         main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, REQUST_CODE);
+                startActivityForResult(intent, 0);
             }
         });
-
-
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 2);
+            }
+        });
+        image3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 3);
+            }
+        });
+        image4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 4);
+            }
+        });
+        image5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 5);
+            }
+        });
+        image6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 6);
+            }
+        });
 
         Button button_upload = findViewById(R.id.upload);
         button_upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                insertToDatabase(recipe_title.getText().toString(), recipe_mat.getText().toString(),
+                        recipe_text.getText().toString());
                 Intent intent = new Intent(RecipewriteActivity.this, RecipeexplanationActivity.class);
                 startActivity(intent);
                 finish();
+
             }
         });
+    }
+    private void insertToDatabase(final String ed1, String ed2, String ed3) {
+        class InsertData extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(RecipewriteActivity.this, "Please Wait", null, true, true);
+            }
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                //Log.d("Tag : ", s); // php에서 가져온 값을 최종 출력함
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            protected String doInBackground(String... params) {
+
+                try {
+                    String edt1Text = (String) params[0];
+                    String edt2Text = (String) params[1];
+                    String edt3Text = (String) params[2];
+
+                    String link = "http://admin0000.dothome.co.kr/insert.php";
+                    String data = URLEncoder.encode("recipe_title", "UTF-8") + "=" + URLEncoder.encode(edt1Text, "UTF-8");
+                    data += "&" + URLEncoder.encode("recipe_material", "UTF-8") + "=" + URLEncoder.encode(edt2Text, "UTF-8");
+                    data += "&" + URLEncoder.encode("recipe_text", "UTF-8") + "=" + URLEncoder.encode(edt3Text, "UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(conn.getOutputStream());
+                    outputStreamWriter.write(data);
+                    outputStreamWriter.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    Log.d("tag : ", sb.toString()); // php에서 결과값을 리턴
+                    return sb.toString();
+
+                } catch (Exception e) {
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+        }
+        InsertData task = new InsertData();
+        task.execute(ed1,ed2,ed3);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUST_CODE) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+
+                try {
+                        InputStream in = getContentResolver().openInputStream(data.getData());
+                        Bitmap img = BitmapFactory.decodeStream(in);
+                        in.close();
+                        main.setImageBitmap(img);
+
+                } catch (Exception e) {
+                }
+            }
+        } else if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
                 try {
                     InputStream in = getContentResolver().openInputStream(data.getData());
-
                     Bitmap img = BitmapFactory.decodeStream(in);
                     in.close();
-
-                    main.setImageBitmap(img);
+                    image1.setImageBitmap(img);
                 } catch (Exception e) {
-
                 }
             }
-        } else if (resultCode == RESULT_CANCELED) {
+        }else if (requestCode == 2) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    image2.setImageBitmap(img);
+                } catch (Exception e) {
+                }
+            }
+        }
+        else if (requestCode == 3) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    image3.setImageBitmap(img);
+                } catch (Exception e) {
+                }
+            }
+        }
+        else if (requestCode == 4) {
+            if (resultCode == RESULT_OK) {
+
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    image4.setImageBitmap(img);
+                } catch (Exception e) {
+                }
+            }
+        }
+        else if (requestCode == 5) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    image5.setImageBitmap(img);
+                } catch (Exception e) {
+                }
+            }
+        }
+        else if (requestCode == 6) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    image6.setImageBitmap(img);
+                } catch (Exception e) {
+                }
+            }
+        }
+        else if (resultCode == RESULT_CANCELED) {
             Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
         }
+        }
     }
-
-}
