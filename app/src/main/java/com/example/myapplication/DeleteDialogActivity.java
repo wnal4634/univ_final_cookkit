55aec0f1bb161754fb9b5e9931272cb0a9d601e9
2005877 <ccom0525ii@naver.com>
2022-08-04 17:28:48 +0900

    검색(미완ㅋ)

---
+++ app/src/main/java/com/example/myapplication/DeleteDialogActivity.java
@@ -0,0 +1,85 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.example.myapplication.Register.DeleteRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DeleteDialogActivity extends Activity {
+    TextView member_id, title1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_delete_dialog);
+
+        Button ok_btn = findViewById(R.id.ok_btn);
+        Button no_btn = findViewById(R.id.no_btn);
+
+        member_id = findViewById(R.id.memberID);
+        title1 = findViewById(R.id.title);
+
+        no_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        ok_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "레시피가 삭제되었습니다.", Toast.LENGTH_LONG).show();
+
+                String title = getIntent().getStringExtra("title");
+                String id = getIntent().getStringExtra("id");
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject( response );
+                            boolean success = jsonObject.getBoolean( "success" );
+
+                            if (success) {
+
+                                Toast.makeText(getApplicationContext(), String.format("삭제"), Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(DeleteDialogActivity.this, Fragment_mypage.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                finish();
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), "실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                DeleteRequest deleteRequest = new DeleteRequest( id, title, responseListener);
+                RequestQueue queue = Volley.newRequestQueue( DeleteDialogActivity.this );
+                queue.add( deleteRequest );
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SearchActivity.java
@@ -0,0 +1,112 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity {
+    View view;
+    RecyclerView recyclerView;
+    ArrayList<MainData> list4 = new ArrayList<>();
+    SearchAdapter saAdapter;
+    TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        //뒤로가기
+        ImageButton btn_back = (ImageButton) findViewById(R.id.back_btn);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        String result2 = getIntent().getStringExtra("result");
+        result = findViewById(R.id.result);
+        result.setText(result2);
+
+        recyclerView = findViewById(R.id.Search_recycler);
+        recyclerView.setHasFixedSize(true);
+        saAdapter = new SearchAdapter(list4);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(saAdapter);
+
+
+
+        String serverUrl = "http://admin0000.dothome.co.kr/Search.php";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, serverUrl, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                list4.clear();
+                saAdapter.notifyDataSetChanged();
+
+                try {
+
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject jsonObject = response.getJSONObject(i);
+
+                        String title = jsonObject.getString("recipe_title");
+                        String category = jsonObject.getString("recipe_category");
+                        String image = jsonObject.getString("image_main");
+
+                        Bitmap image_bit = StringToBitmap(image);
+
+                        MainData mainData = new MainData(title, category, image_bit);
+                        if(title.contains(result2)) {
+                            saAdapter.addItem(mainData);
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    public static Bitmap StringToBitmap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
+}
