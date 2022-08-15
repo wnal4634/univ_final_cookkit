5f0cf2831e3554459173916dfa2c77de93c18dca
2005897 <wnal4634@naver.com>
2022-08-15 16:27:34 +0900

    밀키트 가격 데베 연동

--- app/src/main/java/com/example/myapplication/Fragment_mealDetail.java
+++ app/src/main/java/com/example/myapplication/Fragment_mealDetail.java
@@ -1,18 +1,35 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Fragment_mealDetail extends Fragment {
     private View view;
+    ImageView meal_detail_img;
+    TextView meal_name, meal_explain, meal_sale_date;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +42,11 @@ public class Fragment_mealDetail extends Fragment {
 //        String phone_num = bundle.getString("phone_num");
 //        String post_num = bundle.getString("post_num");
 //        String member_ad = bundle.getString("member_ad");
+
+        meal_detail_img = view.findViewById(R.id.meal_detail_img);
+        meal_name = view.findViewById(R.id.meal_name);
+        meal_explain = view.findViewById(R.id.meal_explain);
+        meal_sale_date = view.findViewById(R.id.meal_sale_date);
 
         Button btn_ordergo = (Button) view.findViewById(R.id.ordergo);
         btn_ordergo.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +68,43 @@ public class Fragment_mealDetail extends Fragment {
 //
 //            }
 //        });
+        String serverUrl = "http://admin0000.dothome.co.kr/meal_ex.php";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, serverUrl, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+
+                    for(int i=0; i< response.length(); i++){
+                        JSONObject jsonObject= response.getJSONObject(i);
+
+                        String title = jsonObject.getString("meal_title");
+                        String ex = jsonObject.getString("meal_text");
+                        String sale = jsonObject.getString("meal_sale_period");
+                        String price = jsonObject.getString("meal_price");
+//                        String image = jsonObject.getString("meal_image");
+//
+//                        Bitmap image_bit = StringToBitmap(image);
+
+                        meal_name.setText(title);
+                        meal_explain.setText(ex);
+                        meal_sale_date.setText(sale);
+//                        meal_detail_img.setImageBitmap(image_bit);
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue= Volley.newRequestQueue(getActivity());
+        requestQueue.add(jsonArrayRequest);
+
         return view;
     }
     @Override
@@ -53,4 +112,14 @@ public class Fragment_mealDetail extends Fragment {
         super.onCreate(savedInstanceState);
         }
 
+    public static Bitmap StringToBitmap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/MealOrderActivity.java
+++ app/src/main/java/com/example/myapplication/MealOrderActivity.java
@@ -3,30 +3,47 @@ package com.example.myapplication;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MealOrderActivity extends AppCompatActivity {
-    private TextView meal_count, meal_total_price, meal_name;
+    private TextView meal_count, meal_total_price, meal_name, meal_price_fix;
     private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
     private EditText postNum, phoneNumber, Address;
     private ImageButton meal_minus, meal_plus;
-    private int count=0;
+    private int count=1;
     String member_id, phone_num, post_num, member_ad = "";
     Button payment;
     static final int SMS_SEND_PERMISSION = 1;
     static final int SMS_RECIVE_PERMISSION = 1;
-    int sum = 1;
-    int price = 3000;
+    int sum = 0;
+    int price = 0;
+    ImageView meal_detail_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,31 +53,77 @@ public class MealOrderActivity extends AppCompatActivity {
         meal_count.setText(count+"");
         meal_plus = findViewById(R.id.meal_plus);
         meal_minus = findViewById(R.id.meal_minus);
+        meal_price_fix = findViewById(R.id.meal_price_fix);
         meal_total_price = findViewById(R.id.meal_total_price);
-        meal_total_price.setText(sum+"원");
+//        meal_total_price.setText(String.valueOf(sum));
+
+        String serverUrl = "http://admin0000.dothome.co.kr/meal_ex.php";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, serverUrl, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                try {
+
+                    for(int i=0; i< response.length(); i++){
+                        JSONObject jsonObject= response.getJSONObject(i);
+
+                        String title = jsonObject.getString("meal_title");
+                        String ex = jsonObject.getString("meal_text");
+                        String sale = jsonObject.getString("meal_sale_period");
+                        String price = jsonObject.getString("meal_price");
+//                        String image = jsonObject.getString("meal_image");
+//
+//                        Bitmap image_bit = StringToBitmap(image);
+
+                        meal_name.setText(title);
+                        meal_total_price.setText(price);
+                        meal_price_fix.setText(price);
+//                        meal_detail_img.setImageBitmap(image_bit);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MealOrderActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+
+
+
 
         meal_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 count++;
+                sum = Integer.parseInt(meal_total_price.getText().toString());
+                int price = Integer.parseInt(meal_total_price.getText().toString());
+                int fix_price = Integer.parseInt(meal_price_fix.getText().toString());
                 meal_count.setText(count+"");
-                sum = price * count;
-                meal_total_price.setText(sum+"원");
+                sum += price;
+                meal_total_price.setText(fix_price * count +"");
             }
         });
 
         meal_minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (count==1) {
-                    meal_total_price.setText(price + "원");
+                sum = Integer.parseInt(meal_total_price.getText().toString());
+                int price = Integer.parseInt(meal_total_price.getText().toString());
+                int fix_price = Integer.parseInt(meal_price_fix.getText().toString());
+                if (count == 1) {
+                    meal_total_price.setText(price+"");
                 } else {
                     if (count > 1) {
                         count--;
-                        meal_count.setText(count + "");
+                        meal_count.setText(count+"");
                     }
                     sum -= price;
-                    meal_total_price.setText(sum + "원");
+                    meal_total_price.setText(fix_price * count+"");
                 }
             }
         });
@@ -69,9 +132,9 @@ public class MealOrderActivity extends AppCompatActivity {
         btn_back_index.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(
-//                        MealOrderActivity.this, Fragment_mealDetail.class);
-//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                Intent intent = new Intent(
+                        MealOrderActivity.this, Fragment_mealDetail.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 finish();
             }
         });
@@ -160,6 +223,17 @@ public class MealOrderActivity extends AppCompatActivity {
                     }
                 }
                 break;
+        }
+    }
+
+    public static Bitmap StringToBitmap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
         }
     }
 
