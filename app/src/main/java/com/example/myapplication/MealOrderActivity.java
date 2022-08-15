3b19f6935606905ec955e522e89fb8faec8c45f1
2005897 <wnal4634@naver.com>
2022-08-15 14:25:03 +0900

    밀키트 가격 음수로 안 떨어지게

--- app/src/main/java/com/example/myapplication/MealOrderActivity.java
+++ app/src/main/java/com/example/myapplication/MealOrderActivity.java
@@ -1,13 +1,5 @@
 package com.example.myapplication;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -17,9 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 import android.widget.TextView;
-import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 public class MealOrderActivity extends AppCompatActivity {
     private TextView meal_count, meal_total_price, meal_name;
     private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
@@ -30,7 +25,7 @@ public class MealOrderActivity extends AppCompatActivity {
     Button payment;
     static final int SMS_SEND_PERMISSION = 1;
     static final int SMS_RECIVE_PERMISSION = 1;
-    int sum = 0;
+    int sum = 1;
     int price = 3000;
 
     @Override
@@ -57,11 +52,16 @@ public class MealOrderActivity extends AppCompatActivity {
         meal_minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(count>0) {
-                    count--;
-                    meal_count.setText(count + "");}
-                sum -= price;
-                meal_total_price.setText(sum+"원");
+                if (count==1) {
+                    meal_total_price.setText(price + "원");
+                } else {
+                    if (count > 1) {
+                        count--;
+                        meal_count.setText(count + "");
+                    }
+                    sum -= price;
+                    meal_total_price.setText(sum + "원");
+                }
             }
         });
 
